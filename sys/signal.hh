800d730502fbf5b162e7546e8c9fae1754fb7783
Ivan Gankevich <igankevich@ya.ru>
2017-03-29 13:32:41 +0300

    Make signal.cc.

--- sys/signal.hh
+++ sys/signal.hh
@@ -2,6 +2,8 @@
 #define SYS_SIGNAL_HH
 
 #include <signal.h>
+#include <ostream>
+#include "bits/check.hh"
 
 namespace sys {
 
@@ -54,54 +56,14 @@ namespace sys {
 	};
 
 	std::ostream&
-	operator<<(std::ostream& out, const signal rhs) {
-		switch (rhs) {
-			case signal::hang_up: out << "hang_up"; break;
-			case signal::keyboard_interrupt: out << "keyboard_interrupt"; break;
-			case signal::quit: out << "quit"; break;
-			case signal::illegal_instruction: out << "illegal_instruction"; break;
-			case signal::abort: out << "abort"; break;
-			case signal::floating_point_exception: out << "floating_point_exception"; break;
-			case signal::kill: out << "kill"; break;
-			case signal::segmentation_fault: out << "segmentation_fault"; break;
-			case signal::broken_pipe: out << "broken_pipe"; break;
-			case signal::alarm: out << "alarm"; break;
-			case signal::terminate: out << "terminate"; break;
-			case signal::user_defined_1: out << "user_defined_1"; break;
-			case signal::user_defined_2: out << "user_defined_2"; break;
-			case signal::child: out << "child"; break;
-			case signal::resume: out << "resume"; break;
-			case signal::stop: out << "stop"; break;
-			case signal::stop_from_terminal: out << "stop_from_terminal"; break;
-			case signal::terminal_input: out << "terminal_input"; break;
-			case signal::terminal_output: out << "terminal_output"; break;
-			case signal::bad_memory_access: out << "bad_memory_access"; break;
-			#ifdef SIGPOLL
-			case signal::poll: out << "poll"; break;
-			#endif
-			case signal::profile: out << "profile"; break;
-			case signal::bad_argument: out << "bad_argument"; break;
-			case signal::breakpoint: out << "breakpoint"; break;
-			case signal::urgent: out << "urgent"; break;
-			case signal::virtual_alarm: out << "virtual_alarm"; break;
-			case signal::cpu_time_limit_exceeded: out << "cpu_time_limit_exceeded"; break;
-			case signal::file_size_limit_exceeded: out << "file_size_limit_exceeded"; break;
-			#ifdef SIGSTKFLT
-			case signal::coprocessor_stack_fault: out << "coprocessor_stack_fault"; break;
-			#endif
-			#ifdef SIGPWR
-			case signal::power_failure: out << "power_failure"; break;
-			#endif
-			#ifdef SIGWINCH
-			case signal::window_resize: out << "window_resize"; break;
-			#endif
-		}
-		out << '(' << signal_type(rhs) << ')';
-		return out;
-	}
+	operator<<(std::ostream& out, const signal rhs);
 
 	struct signal_action: public sigaction_type {
+
+		inline
 		signal_action() = default;
+
+		inline
 		signal_action(void (*func)(int)) noexcept {
 			this->sa_handler = func;
 		}
