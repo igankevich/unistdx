3bb27bc7fd42b55d66a7ba29486bd51458eddbc1
Ivan Gankevich <igankevich@ya.ru>
2017-10-12 22:12:55 +0300

    Implement event poller using epoll.

--- src/test/poller_test.cc
+++ src/test/poller_test.cc
@@ -1,8 +1,13 @@
+#include <unistdx/io/poller2>
 #include <unistdx/io/poller>
+
 #include "semaphore_wait_test.hh"
 #include "make_types.hh"
 
-TYPED_TEST_CASE(SemaphoreWaitTest, MAKE_TYPES(sys::event_poller<void*>));
+TYPED_TEST_CASE(
+	SemaphoreWaitTest,
+	MAKE_TYPES(sys::event_poller<void*>, sys::event_poller2)
+);
 
 TYPED_TEST(SemaphoreWaitTest, EventPollerWaitUntil) {
 	this->test_wait_until();
