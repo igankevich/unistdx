a02e81cfb942c2c01b08a7f4038d899e08ebe512
Ivan Gankevich <igankevich@ya.ru>
2017-10-12 18:28:41 +0300

    Epoll wrapper WIP.

---
+++ src/unistdx/io/epoll_event.cc
@@ -0,0 +1,16 @@
+#include "epoll_event"
+
+#include <ostream>
+#include <unistdx/base/make_object>
+
+std::ostream&
+sys::operator<<(std::ostream& out, const epoll_event& rhs) {
+	const char ev[] = {
+		(rhs.in() ? 'r' : '-'),
+		(rhs.out() ? 'w' : '-'),
+		(rhs.hup() ? 'c' : '-'),
+		(rhs.err() ? 'e' : '-'),
+		0
+	};
+	return out << make_object("fd", rhs.fd(), "ev", ev);
+}
