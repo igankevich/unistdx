506f4ea5110904dfa553fe4d6ec97d012214a502
Ivan Gankevich <igankevich@ya.ru>
2017-11-13 00:16:23 +0300

    Switch to local buffers for log_message.

--- src/unistdx/base/log_message.cc
+++ src/unistdx/base/log_message.cc
@@ -1,20 +1,8 @@
 #include "log_message"
 
-#include <unistdx/io/fildesbuf>
-
-namespace {
-
-	typedef sys::basic_fildesbuf<char,std::char_traits<char>,sys::fd_type>
-	    fildesbuf_type;
-
-	thread_local fildesbuf_type threadbuf(STDERR_FILENO, 0, 256);
-
-}
-
-thread_local sys::osysstream sys::log(&threadbuf);
-
 sys::log_message::~log_message() {
 	char ch = '\n';
-	threadbuf.sputn(&ch, 1);
-	threadbuf.pubflush();
+	this->_str.buf().sputn(&ch, 1);
+	this->_str.buf().pubflush();
 }
+
