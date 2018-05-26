f35408ab32b119bccab0a144211f234154866385
Ivan Gankevich <igankevich@ya.ru>
2018-05-26 19:06:44 +0300

    Improve documentation.
    - Configure doxygen layout and menu.
    - Add sys::this_proces::execute* examples.

---
+++ doc/examples/execute.cc
@@ -0,0 +1,19 @@
+#include <unistdx/ipc/execute>
+
+/**
+\example execute.cc
+\details
+The following code executes \c errno to display description
+of error code 42 in Russian locale.
+*/
+int main() {
+	/// [Execute child process]
+	sys::argstream args;
+	args.append("/bin/errno");
+	args.append(42);
+	sys::argstream env;
+	env.append("LANG=ru_RU.UTF-8");
+	sys::this_process::execute(args, env);
+	/// [Execute child process]
+	return 0;
+}
