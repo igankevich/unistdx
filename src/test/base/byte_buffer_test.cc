d312ef3b79e63b0313840e305fa35b2d66807cb3
Ivan Gankevich <igankevich@ya.ru>
2018-06-05 11:38:32 +0300

    Configure llvm-cov and gcovr for coverage.

--- src/test/base/byte_buffer_test.cc
+++ src/test/base/byte_buffer_test.cc
@@ -4,7 +4,8 @@
 #include <unistdx/base/check>
 
 TEST(byte_buffer, resize) {
+	const size_t max0 = sys::byte_buffer::max_size();
 	sys::byte_buffer buf(4096);
-	EXPECT_THROW(buf.resize(sys::byte_buffer::max_size()), sys::bad_call);
-	EXPECT_THROW(sys::byte_buffer(sys::byte_buffer::max_size()), sys::bad_call);
+	EXPECT_THROW(buf.resize(max0), sys::bad_call);
+	EXPECT_THROW(sys::byte_buffer b(max0), sys::bad_call);
 }
