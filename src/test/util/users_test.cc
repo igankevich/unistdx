e9c0353deee849672b81bf84de757be10ff38ce4
Ivan Gankevich <igankevich@ya.ru>
2018-06-16 00:12:28 +0300

    Fix get entity error.

--- src/test/util/users_test.cc
+++ src/test/util/users_test.cc
@@ -54,43 +54,24 @@ TEST(Group, Enumerate) {
 
 TEST(User, FindBy) {
 	errno = 0;
-	std::clog << 1 << std::endl;
 	sys::user u1, u2, u3;
-	std::clog << 2 << std::endl;
 	bool success;
-	std::clog << 3 << std::endl;
 	success = sys::find_user(sys::this_process::user(), u1);
-	std::clog << 4 << std::endl;
 	EXPECT_TRUE(success);
-	std::clog << 5 << std::endl;
 	EXPECT_EQ(sys::this_process::user(), u1.id());
-	std::clog << 6 << std::endl;
 	EXPECT_EQ(sys::this_process::group(), u1.group_id());
-	std::clog << 7 << std::endl;
 	EXPECT_STREQ(std::getenv("HOME"), u1.home());
-	std::clog << 8 << std::endl;
 	EXPECT_STREQ(std::getenv("SHELL"), u1.shell());
-	std::clog << 9 << std::endl;
 	success = sys::find_user(u1.name(), u2);
-	std::clog << 10 << std::endl;
 	EXPECT_TRUE(success);
-	std::clog << 11 << std::endl;
 	EXPECT_EQ(u1, u2);
-	std::clog << 12 << std::endl;
 	EXPECT_STREQ(u1.password(), u2.password());
-	std::clog << 13 << std::endl;
 	EXPECT_STREQ(u1.real_name(), u2.real_name());
-	std::clog << 14 << std::endl;
 	sys::user copy(std::move(u2));
-	std::clog << 15 << std::endl;
 	EXPECT_EQ(u1, copy);
-	std::clog << 16 << std::endl;
 	test::stream_insert_starts_with(u1.name(), u1);
-	std::clog << 17 << std::endl;
 	success = sys::find_user("a", u3);
-	std::clog << 18 << std::endl;
 	EXPECT_FALSE(success);
-	std::clog << 19 << std::endl;
 }
 
 TEST(Group, FindBy) {
