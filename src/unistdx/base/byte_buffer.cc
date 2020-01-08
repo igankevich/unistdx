aa29683cdf1b2b3c69ac673e076b55d1958dbffb
Ivan Gankevich <i.gankevich@spbu.ru>
2020-01-08 21:37:34 +0300

    Update byte buffer to resemble java.nio.ByteBuffer.

--- src/unistdx/base/byte_buffer.cc
+++ src/unistdx/base/byte_buffer.cc
@@ -1,7 +1,7 @@
-#include <unistdx/base/byte_buffer>
-
 #include <cassert>
+#include <cstring>
 
+#include <unistdx/base/byte_buffer>
 #include <unistdx/base/check>
 #include <unistdx/bits/mman>
 
@@ -18,7 +18,7 @@ namespace {
 }
 
 sys::byte_buffer::byte_buffer(size_type size):
-_size(size) {
+_size(size), _limit(size) {
     if (this->_size > 0) {
         void* ptr = ::mmap(
             nullptr,
@@ -52,5 +52,48 @@ sys::byte_buffer::resize(size_type new_size) {
     UNISTDX_CHECK2(ptr, MAP_FAILED);
     this->_data = static_cast<value_type*>(ptr);
     this->_size = new_size;
+    this->_limit = new_size;
     init_pages(this->_data, this->_size);
+}
+
+auto
+sys::byte_buffer::write(const_pointer src, size_type n) -> size_type {
+    while (n > remaining()) { grow(); }
+    std::memcpy(data()+position(), src, n);
+    this->_position += n;
+    return n;
+}
+
+auto
+sys::byte_buffer::read(pointer dst, size_type n) -> size_type {
+    if (n > remaining()) { throw std::range_error("sys::byte_buffer::read"); }
+    std::memcpy(dst, data()+position(), n);
+    this->_position += n;
+    return n;
+}
+
+void
+sys::byte_buffer::peek(pointer dst, size_type n) {
+    std::memcpy(dst, data()+position(), n);
+}
+
+void
+sys::byte_buffer::bump(size_type n) {
+    while (n > remaining()) { grow(); }
+    this->_position += n;
+}
+
+void
+sys::byte_buffer::compact() {
+    auto n = remaining();
+    std::memmove(data(), data()+position(), n);
+    this->_position = n;
+    this->_limit = size();
+}
+
+void
+sys::byte_buffer::clear() {
+    this->_position = 0;
+    this->_limit = size();
+    std::memset(data(), 0, size());
 }
