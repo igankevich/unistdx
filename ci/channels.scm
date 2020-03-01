df95fd5d9e9e7797ef99e295691777a2624da3c9
Ivan Gankevich <i.gankevich@spbu.ru>
2020-03-01 16:35:16 +0300

    Make CI script.

---
+++ ci/channels.scm
@@ -0,0 +1,14 @@
+(append
+  (list
+	(channel
+	  (name 'stables)
+	  (url "https://git.cmmshq.ru/git/igankevich/guix.git"))
+	(channel
+	  (name 'nonguix)
+	  (url "https://git.cmmshq.ru/git/infrastructure/nonguix.git")
+	  (commit "42fa1c0fdc28eb8fe38ecc1288a541db5161faf4"))
+	(channel
+	  (name 'guix)
+	  (url "https://git.cmmshq.ru/git/infrastructure/guix.git")
+	  (commit "71804546b182b8300334df6fb23d207d5e5ba61c"))))
+
