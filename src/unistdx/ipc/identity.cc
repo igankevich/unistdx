0652a8d39e2859381a3f9c35f628c478fc1c7141
Ivan Gankevich <i.gankevich@spbu.ru>
2020-05-15 19:26:52 +0300

    Check the current uid/gid before setting new ones.

--- src/unistdx/ipc/identity.cc
+++ src/unistdx/ipc/identity.cc
@@ -2,10 +2,13 @@
 
 void
 sys::this_process::set_identity(uid_type uid, gid_type gid) {
+    // Change uid/gid only if they are different from the current ones.
+    // This check is needed when we try to set oveflow_uid/overflow_gid.
+    uid_type old_uid = user();
     gid_type old_gid = group();
-    group(gid);
-    if (::setuid(uid) == -1) {
-        setgid(old_gid);
+    if (old_gid != gid) { group(gid); }
+    if (old_uid != uid && ::setuid(uid) == -1) {
+        if (old_gid != gid) { setgid(old_gid); }
         UNISTDX_THROW_BAD_CALL();
     }
 }
