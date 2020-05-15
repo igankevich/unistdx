65e7a6071f6bff6bf45959964d5dff0b54bf9a50
Ivan Gankevich <i.gankevich@spbu.ru>
2020-05-15 21:37:43 +0300

    Update channels and slurm script.

--- ci/channels.scm
+++ ci/channels.scm
@@ -4,7 +4,7 @@
 	  (name 'stables)
 	  (url "https://git.cmmshq.ru/git/igankevich/guix.git"))
 	(channel
-	  (name 'nonguix)
+	  (name 'cmmshq-nonguix)
 	  (url "https://git.cmmshq.ru/git/infrastructure/nonguix.git")
 	  (commit "de98abc49c2fbad337e45abdb20994ce45bd3295"))
 	(channel
