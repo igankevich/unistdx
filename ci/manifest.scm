b01986d54d78964eb3963e91486966a7107290b8
Ivan Gankevich <i.gankevich@spbu.ru>
2020-11-25 00:34:46 +0300

    Bump version.

--- ci/manifest.scm
+++ ci/manifest.scm
@@ -16,7 +16,6 @@
       (@ (gnu packages python-xyz) python-chardet)
       (@ (gnu packages code) lcov)
       (@ (gnu packages gcovr) python-gcovr)
-      (@ (gnu packages libunwind) libunwind)
       (@ (gnu packages elf) elfutils)
       (@ (gnu packages compression) xz)
       (list (@ (gnu packages llvm) clang-10) "extra") ;; clang-tidy
